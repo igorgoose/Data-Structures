9cf2dafb3e128acb1dc1fe358cfc66642c8855b5
igorgoose <igotgood01@gmail.com>
2020-08-30 23:14:47 +0300

    Added AVL Binary Tree with tests

---
+++ src/main/java/by/schepov/tree/AVLBinaryTree.java
@@ -0,0 +1,236 @@
+package by.schepov.tree;
+
+public class AVLBinaryTree {
+
+    private Node root;
+    private int size;
+
+    private static class Node {
+
+        private int key;
+        private Node leftNode;
+        private Node rightNode;
+        private Node parentNode;
+        private int height = 0;
+
+        private Node() {
+
+        }
+
+        private Node(int key) {
+            this.key = key;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public void setKey(int key) {
+            this.key = key;
+        }
+
+        public Node getLeftNode() {
+            return leftNode;
+        }
+
+        public void setLeftNode(Node leftNode) {
+            this.leftNode = leftNode;
+        }
+
+        public Node getRightNode() {
+            return rightNode;
+        }
+
+        public void setRightNode(Node rightNode) {
+            this.rightNode = rightNode;
+        }
+
+        public Node getParentNode() {
+            return parentNode;
+        }
+
+        public void setParentNode(Node parentNode) {
+            this.parentNode = parentNode;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+
+    private int getNodeHeight(Node node) {
+        return node != null ? node.getHeight() : 0;
+    }
+
+    private int getBalanceFactor(Node node) {
+        return getNodeHeight(node.getRightNode()) - getNodeHeight(node.getLeftNode());
+    }
+
+    private void fixHeight(Node node) {
+        int leftHeight = node.getLeftNode() == null ? 0 : node.getLeftNode().getHeight();
+        int rightHeight = node.getRightNode() == null ? 0 : node.getRightNode().getHeight();
+        node.setHeight(Math.max(leftHeight, rightHeight) + 1);
+    }
+
+    private Node rotateLeft(Node node) {
+        Node right = node.getRightNode();
+        Node parent = node.getParentNode();
+        node.setRightNode(right.getLeftNode());
+        if (right.getLeftNode() != null) {
+            right.getLeftNode().setParentNode(node);
+        }
+        right.setLeftNode(node);
+        node.setParentNode(right);
+        right.setParentNode(parent);
+        if (parent != null) {
+            if (node == parent.getLeftNode()) {
+                parent.setLeftNode(right);
+            } else {
+                parent.setRightNode(right);
+            }
+        }
+        fixHeight(node);
+        fixHeight(right);
+        return right;
+    }
+
+    private Node rotateRight(Node node) {
+        Node left = node.getLeftNode();
+        Node parent = node.getParentNode();
+        node.setLeftNode(left.getRightNode());
+        if (left.getRightNode() != null) {
+            left.getRightNode().setParentNode(node);
+        }
+        left.setRightNode(node);
+        node.setParentNode(left);
+        left.setParentNode(parent);
+        if (parent != null) {
+            if (node == parent.getLeftNode()) {
+                parent.setLeftNode(left);
+            } else {
+                parent.setRightNode(left);
+            }
+        }
+        fixHeight(node);
+        fixHeight(left);
+        return left;
+    }
+
+    private Node balanceNode(Node node) {
+        fixHeight(node);
+        int balanceFactor = getBalanceFactor(node);
+        if (balanceFactor == 2) {
+            if (getBalanceFactor(node.getRightNode()) < 0) {
+                rotateRight(node.getRightNode());
+            }
+            return rotateLeft(node);
+        }
+        if (balanceFactor == -2) {
+            if (getBalanceFactor(node.getLeftNode()) > 0) {
+                rotateLeft(node.getLeftNode());
+            }
+            return rotateRight(node);
+        }
+        return node;
+    }
+
+    private Node addKeyInSubtree(Node root, int key) {
+        if (root == null) {
+            return new Node(key);
+        }
+        if (key < root.getKey()) {
+            Node newLeft = addKeyInSubtree(root.getLeftNode(), key);
+            root.setLeftNode(newLeft);
+            newLeft.setParentNode(root);
+        } else if (key > root.getKey()) {
+            Node newRight = addKeyInSubtree(root.getRightNode(), key);
+            root.setRightNode(newRight);
+            newRight.setParentNode(root);
+        }
+        return balanceNode(root);
+    }
+
+    public void addKey(int key) {
+        root = addKeyInSubtree(root, key);
+    }
+
+    private Node findMinInSubtree(Node root) {
+        return root.getLeftNode() == null ? root : findMinInSubtree(root.getLeftNode());
+    }
+
+    private Node removeMinInSubtree(Node root) {
+        if (root.getLeftNode() == null) {
+            return root.getRightNode();
+        }
+        root.setLeftNode(removeMinInSubtree(root.getLeftNode()));
+        root.getLeftNode().setParentNode(root);
+        return balanceNode(root);
+    }
+
+    private Node removeKeyInSubtree(Node root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (key < root.getKey()) {
+            Node newLeft = removeKeyInSubtree(root.getLeftNode(), key);
+            root.setLeftNode(newLeft);
+            if (newLeft != null) {
+                newLeft.setParentNode(root);
+            }
+        } else if (key > root.getKey()) {
+            Node newRight = removeKeyInSubtree(root.getRightNode(), key);
+            root.setRightNode(newRight);
+            if (newRight != null) {
+                newRight.setParentNode(root);
+            }
+        } else {
+            Node right = root.getRightNode();
+            Node left = root.getLeftNode();
+            if (right == null) {
+                return left;
+            }
+            Node min = findMinInSubtree(right);
+            min.setRightNode(removeMinInSubtree(right));
+            min.getRightNode().setParentNode(min);
+            min.setLeftNode(left);
+            left.setParentNode(min);
+            return balanceNode(min);
+        }
+        return balanceNode(root);
+    }
+
+    public void removeKey(int key) {
+        root = removeKeyInSubtree(root, key);
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        buildString(root, str);
+        return str.toString().trim();
+    }
+
+    private void buildString(Node node, StringBuilder str) {
+        if (node == null) {
+            return;
+        }
+        str.append(node.getKey()).append(" ");
+        buildString(node.getLeftNode(), str);
+        buildString(node.getRightNode(), str);
+    }
+
+}
---
+++ src/test/java/by/schepov/tree/AVLBinaryTreeTest.java
@@ -0,0 +1,76 @@
+package by.schepov.tree;
+
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+
+public class AVLBinaryTreeTest {
+
+    @Test
+    public void defaultConstructorTest(){
+        assertEquals("", new AVLBinaryTree().toString());
+    }
+
+    @Test
+    public void insertKeyTestRightSide(){
+        AVLBinaryTree tree = new AVLBinaryTree();
+        tree.addKey(1);
+        tree.addKey(2);
+        tree.addKey(5);
+        tree.addKey(4);
+        tree.addKey(6);
+        tree.addKey(3);
+        assertEquals("4 2 1 3 5 6", tree.toString());
+    }
+
+    @Test
+    public void insertKeyTestLeftSide(){
+        AVLBinaryTree tree = new AVLBinaryTree();
+        tree.addKey(6);
+        tree.addKey(5);
+        tree.addKey(2);
+        tree.addKey(3);
+        tree.addKey(1);
+        tree.addKey(4);
+        assertEquals("3 2 1 5 4 6", tree.toString());
+    }
+
+    @Test
+    public void removeKeyTestNoRightSubtree() {
+        AVLBinaryTree tree = new AVLBinaryTree();
+        tree.addKey(6);
+        tree.addKey(5);
+        tree.addKey(2);
+        tree.addKey(3);
+        tree.addKey(1);
+        tree.addKey(4);
+        tree.removeKey(1);
+        assertEquals("3 2 5 4 6", tree.toString());
+    }
+
+    @Test
+    public void removeKeyTestWithRightSubtree() {
+        AVLBinaryTree tree = new AVLBinaryTree();
+        tree.addKey(7);
+        tree.addKey(6);
+        tree.addKey(2);
+        tree.addKey(3);
+        tree.addKey(1);
+        tree.addKey(4);
+        tree.addKey(5);
+        tree.removeKey(3);
+        assertEquals("4 2 1 6 5 7", tree.toString());
+    }
+
+    @Test
+    public void removeKeyTestOffBalance() {
+        AVLBinaryTree tree = new AVLBinaryTree();
+        tree.addKey(4);
+        tree.addKey(2);
+        tree.addKey(6);
+        tree.addKey(5);
+        tree.addKey(7);
+        tree.removeKey(2);
+        assertEquals("6 4 5 7", tree.toString());
+    }
+}
